fead6e9dbd69e113ff56d110cc253e0e5df3b562
MohammadHossein <@Jahanshahi.com>
2025-06-01 13:50:27 +0430

    Initail commit

---
+++ WebApplication30/Context/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreProductCRUD.Models;
+
+
+namespace WebApplication30.DbContext;
+
+
+    public class StoreContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
